35804d653fbb1aa341e5c6a89282dd0885a5fd07
IamAlchemist <wizard.lee@qq.com>
2016-04-08 15:13:47 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/productId/', function(req, res, next) {
+    var result = {"productId" : 80}
+    var response = { "errCode" : 0,
+		    "result" : result }
+    res.send(response)
+});
+
+router.get('/product/articles', function(req, res, next) {
+    var result = { "productId": req.query.productId,
+		   "articles" : "contents" }
+    var response = { "errCode" : 0 ,
+		     "result" : result }
+    res.send(response)
+});
+	   
+
+	   
+module.exports = router;
